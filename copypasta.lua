02fe10e5545c641c4fe9b6897b14f2a93c4a81b4
Hugo O. Rivera <hugo@roguh.com>
2023-04-13 10:46:30 -0600

    Much improved keybindings

--- copypasta.lua
+++ copypasta.lua
@@ -1,13 +1,11 @@
 -- Paste from system clipboard with vis-clipboard
 vis:map(vis.modes.NORMAL, '<C-v>', '"+p')
-vis:map(vis.modes.INSERT, '<C-v>', function()
-    -- Put register content after cursor from system clipboard with vis-clipoard
-    vis.mode = vis.modes.NORMAL
-    vis:feedkeys('<C-v>')
-end)
+-- Put register content after cursor from system clipboard with vis-clipoard
+-- Change the 'a' to an 'i' if you want it before the cursor
+vis:map(vis.modes.INSERT, '<C-v>', '<Escape>"+pa')
 
 -- Put selection content into system clipboard
-vis:map(vis.modes.VISUAL_LINE, '<C-c>', function() vis:feedkeys(':>vis-clipboard --copy') end)
-
+vis:map(vis.modes.VISUAL_LINE, '<C-c>', function() vis:feedkeys(':>vis-clipboard --copy<Enter>') end)
 -- TODO this doesn't work with multiple cursors, just gets last selection
-vis:map(vis.modes.VISUAL, '<C-c>', function() vis:feedkeys(':>vis-clipboard --copy') end)
+vis:map(vis.modes.VISUAL, '<C-c>', function() vis:feedkeys(':>vis-clipboard --copy<Enter>') end)
+
